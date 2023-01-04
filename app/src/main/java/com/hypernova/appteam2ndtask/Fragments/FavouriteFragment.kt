9f2236adce380bd0f5f86712589e4b09269deb8e
Dark-Knight11 <Dark-Knight11@users.noreply.github.com>
2023-01-04 14:48:12 +0000

    Auto-modify files by ktlint

--- app/src/main/java/com/hypernova/appteam2ndtask/Fragments/FavouriteFragment.kt
+++ app/src/main/java/com/hypernova/appteam2ndtask/Fragments/FavouriteFragment.kt
@@ -21,15 +21,6 @@ class FavouriteFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
 
-
-
-
-
-
-
-
-
-
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
